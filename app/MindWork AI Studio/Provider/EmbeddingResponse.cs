c490e0c66818bb2470294613593797796a537be7
Thorsten Sommer <SommerEngineering@users.noreply.github.com>
2026-02-20 15:18:48 +0100

    Make properties immutable

--- app/MindWork AI Studio/Provider/EmbeddingResponse.cs
+++ app/MindWork AI Studio/Provider/EmbeddingResponse.cs
@@ -2,9 +2,13 @@
 
 public sealed record EmbeddingResponse
 {
-    public string? Id { get; set; }
-    public string? Object { get; set; }
-    public List<EmbeddingData>? Data { get; set; }
-    public string? Model { get; set; }
-    public Usage? Usage { get; set; }
+    public string? Id { get; init; }
+    
+    public string? Object { get; init; }
+    
+    public List<EmbeddingData>? Data { get; init; }
+    
+    public string? Model { get; init; }
+    
+    public EmbeddingUsage? Usage { get; init; }
 }
